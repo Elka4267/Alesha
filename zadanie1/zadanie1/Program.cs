96ed8efac547b3fa026847695fb10fe38507b88b
Elka4267 <48008629+Elka4267@users.noreply.github.com>
2019-03-19 17:58:10 +0300

    toha chek

---
+++ zadanie1/zadanie1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            { 
+                var arr2 = new[] { "Hello Anton!"};
+                Console.WriteLine( "{0}", arr2);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
